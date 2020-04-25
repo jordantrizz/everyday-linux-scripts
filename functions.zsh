1e50e44bc2513e68989518295ce919e13b2aadf0
Jordan Trask <jtrask@gmail.com>
2020-04-25 14:23:46 -0700

    Forgot sudo

--- functions.zsh
+++ functions.zsh
@@ -6,5 +6,5 @@ ultb_install () {
 
 ubuntu-netselect () {
 	wget http://ftp.us.debian.org/debian/pool/main/n/netselect/netselect_0.3.ds1-28+b1_amd64.deb
-	dpkg -i netselect_0.3.ds1-28+b1_amd64.deb
+	sudo dpkg -i netselect_0.3.ds1-28+b1_amd64.deb
 }
