4a6d77eefe03dd64f2879c7aeaec64e538690a7d
Jordan Trask <jtrask@gmail.com>
2020-04-29 10:42:37 -0700

    Updated functions.sh and created setup-automysqlbackup

--- functions.zsh
+++ functions.zsh
@@ -8,3 +8,8 @@ ubuntu-netselect () {
 	wget http://ftp.us.debian.org/debian/pool/main/n/netselect/netselect_0.3.ds1-28+b1_amd64.deb
 	sudo dpkg -i netselect_0.3.ds1-28+b1_amd64.deb
 }
+
+setup-automysqlbackup () {
+	cd $ULTB/AutoMySQLBackup
+	./install
+}
